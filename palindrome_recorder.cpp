c79d12b5783338e84d70bd27921e77b38d366229
vijay kumar chaurasiya <118093154+vijay-kc@users.noreply.github.com>
2024-01-24 02:57:05 +0530

    Add files via upload

---
+++ palindrome_recorder.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int main (){
+ string s;
+    cin >> s;
+
+    vector<int> a(26);
+    for(char c : s) a[c - 'A']++;
+    
+    int check = 0;
+    for(int i = 0; i < 26; i++) check += (a[i] % 2);
+    if(check > 1){
+        cout << "NO SOLUTION";
+        return 0;
+    }
+ 
+    string result;
+    for (int i = 0; i < 26; i++){
+        if(!(a[i]%2)){
+            for(int j = 0; j < a[i]/2; j++) result.push_back(i + 'A');
+        }
+    }
+    for (int i = 0; i < 26; i++){
+        if(a[i]%2){
+            for(int j = 0; j < a[i]; j++) result.push_back(i + 'A');
+        }
+    }
+    for (int i = 25; i >= 0; i--){
+        if(!(a[i]%2)){
+            for(int j = 0; j < a[i]/2; j++) result.push_back(i + 'A');
+        }
+    }
+
+    cout << result << endl;
+   
+return 0;
+}
