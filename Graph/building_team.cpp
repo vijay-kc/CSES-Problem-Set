d838711e326d2ac1d3d80250f073941865822e39
vijay kumar chaurasiya <vijaykumarchaurasiya999@gmail.com>
2025-11-24 18:45:00 +0530

    Adding building team problem

---
+++ Graph/building_team.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+void bfs(int s,vector<vector<int>>&adj,vector<int>&team){
+    team[s]=1;
+    queue<int> q;
+    q.push(s);
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+        for (auto v : adj[u])
+        {
+            if (team[v] == -1)
+            {   if(team[u]==1){
+                    team[v] = 2;
+                }else{
+                    team[v]=1;
+                }
+                q.push(v);
+            }else if(team[v]==team[u]){
+                cout<<"IMPOSSIBLE"<<endl;
+                exit(0);
+            }
+        }
+    }
+}
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> adj(n+1);
+    int a, b;
+    for (int i = 0; i < m; i++)
+    {
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    vector<int> team(n+1,-1);
+
+    
+
+    for (int i=1;i<=n;i++){
+        if(team[i]==-1)bfs(i,adj,team);
+    }
+    for (int i=1;i<=n;i++){
+        cout << team[i] << " ";
+    }
+    
+    return 0;
+}
