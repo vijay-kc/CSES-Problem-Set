d5e8249519f3bf6db8715e4a0b70290b64a29797
vijay kumar chaurasiya <vijaykumarchaurasiya999@gmail.com>
2025-11-28 16:24:00 +0530

    Adding book shop problem

---
+++ DP/book_shop.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long solve(vector<long long>&price,vector<long long>&pages,int x,int i,vector<vector<long long>>&dp){
+   if (x == 0 || i == price.size()) return 0;
+   if(dp[i][x]!=-1)return dp[i][x];
+    if(price[i]>x){
+       return dp[i][x]=solve(price,pages,x,i+1,dp);
+    }
+    return dp[i][x]=max(pages[i]+solve(price,pages,x-price[i],i+1,dp),solve(price,pages,x,i+1,dp));
+}
+int main (){
+    long long n,x;
+    cin>>n>>x;
+    vector<long long>price(n);
+    vector<long long>pages(n);
+    for(long long i=0;i<n;i++){
+        cin>>price[i];
+    }
+    for(long long i=0;i<n;i++){
+        cin>>pages[i];
+    }
+    vector<vector<long long>> dp(n, vector<long long>(x + 1, -1));
+    cout<<solve(price,pages,x,0,dp);
+return 0;
+}
+// int main() {
+//     long long n, x;
+//     cin >> n >> x;  
+
+//     vector<long long> price(n);
+//     vector<long long> pages(n);
+
+//     for (long long i = 0; i < n; i++) {
+//         cin >> price[i];  
+//     }
+
+//     for (long long i = 0; i < n; i++) {
+//         cin >> pages[i];  
+//     }
+//     vector<long long>dp(x+1,0);
+//     for (long long i = 0; i < n; i++) {
+        
+//         for (long long C = x; C > 0; C--) {
+            
+//             if (price[i] > C)
+//                 continue;
+//             dp[C] = max(dp[C], pages[i] + dp[C - price[i]]);
+//         }
+//     }
+//     cout<< dp[x];
+//     return 0;
+// }
